14a3375b979b44a1c0c382dc81c0feac321c7375
Vasily Belosludcev <bupy765@gmail.com>
2017-12-15 17:06:52 +0500

    added tests

---
+++ tests/methods/constructor.js
@@ -0,0 +1,28 @@
+describe('constructor', function () {
+  it('must work with element selector', function (done) {
+    // eslint-disable-next-line no-new
+    new window.AnimateScroll('.break-3', {
+      onFinish: function () {
+        done();
+      }
+    })
+  })
+
+  it('must work with object of target', function (done) {
+    // eslint-disable-next-line no-new
+    new window.AnimateScroll(document.querySelector('.break-3'), {
+      onFinish: function () {
+        done();
+      }
+    })
+  })
+
+  it('must work with pixels', function (done) {
+    // eslint-disable-next-line no-new
+    new window.AnimateScroll(200, {
+      onFinish: function () {
+        done();
+      }
+    })
+  })
+})
